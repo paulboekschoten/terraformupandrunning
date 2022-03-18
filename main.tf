1d9b1715f258e51566192f5f3e12158647810aca
Paul Boekschoten <paul.boekschoten@hashicorp.com>
2022-03-18 16:08:02 +0100

    changed ami

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "tf-running-paul" {
-  ami = "ami-0960de83329d12f2f"
+  ami = "ami-0c6ebbd55ab05f070"
   instance_type = "t2.micro"
   tags = {
       Name = "terraform-example-paul"
