aa7ae9e10ee1e7b196e1a40abd3326e08aa7034e
Paul Boekschoten <paul.boekschoten@hashicorp.com>
2022-03-18 17:00:03 +0100

    webserver unreachable p55

--- main.tf
+++ main.tf
@@ -5,7 +5,27 @@ provider "aws" {
 resource "aws_instance" "tf-running-paul" {
   ami = "ami-0c6ebbd55ab05f070"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.tf-instance-paul.id]
+  
   tags = {
       Name = "terraform-example-paul"
+  }
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+}
+
+resource "aws_security_group" "tf-instance-paul" {
+  name = "terraform-example-paul"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
